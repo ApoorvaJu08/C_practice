6f5ddcc0f2e57ee1695295b4fc02ac99991ef595
ApoorvaJu08 <apoorva10339@gmail.com>
2019-07-31 07:21:27 +0530

    armstrong

---
+++ basic/armstrong.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int n, r, sum = 0, tmp;
+    printf("Enter the number : \n");
+    scanf("%d", &n);
+    tmp = n;
+    while(n > 0)
+    {
+        r = n % 10;
+        sum = sum + (r * r * r);
+        n = n / 10;
+    }
+    if(tmp == sum)
+        printf("It is an armstrong number\n");
+    else
+    {
+        printf("Not an armstrong number\n");
+    }
+    return 0;
+}
