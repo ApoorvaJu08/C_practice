ae118d63620599d8867f5eaa8e6193b34a3ad222
ApoorvaJu08 <apoorva10339@gmail.com>
2019-09-13 16:13:16 +0530

    new

---
+++ Most_common_printing_patterns/hollow_sq.c
@@ -0,0 +1,25 @@
+// Program to print solid square
+#include <stdio.h>
+void solid_rectangle(int n, int m) {
+    int i, j;
+    for(i = 1; i <= n; i++)
+    {
+        for(j = 1; j <= m; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int rows, columns;
+    printf("\nEnter the number of rows: ");
+    scanf("%d", &rows);
+    printf("\nEnter the number of columns: ");
+    scanf("%d", &columns);
+    printf("\n");
+    solid_rectangle(rows, columns);
+    return 0;
+}
