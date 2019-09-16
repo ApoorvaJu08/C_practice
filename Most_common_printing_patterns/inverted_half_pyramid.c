d9e988326ff02c568e3aed8368ed368273706d5e
ApoorvaJu08 <apoorva10339@gmail.com>
2019-09-16 10:07:52 +0530

    inverted half pyramid

---
+++ Most_common_printing_patterns/inverted_half_pyramid.c
@@ -0,0 +1,17 @@
+// C program to design a invverted half pyramid
+#include <stdio.h>
+int main()
+{
+    int i, j, n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    for(i = n; i >= 1; --i)
+    {
+        for(j = 1; j <= i; ++j)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
